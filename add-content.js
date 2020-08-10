c60f9d8bff073d8ec0f65537ade053d22b1e6855
vishalrimal <vishalrimal@users.noreply.github.com>
2020-08-10 18:58:49 +0530

    added more functions

--- add-content.js
+++ add-content.js
@@ -1,8 +1,32 @@
-function calcAdd(a, b) {
+function calc(a, b, d) {
   var a = parseInt(document.querySelector("#first").value);
   var b = parseInt(document.querySelector("#second").value);
-  var c = a + b;
+  var d = document.querySelector("#operator").value;
   var bb = document.querySelector("#third");
-  bb.innerHTML = "The sum of the two numbers is : " + c;
-  return c;
+
+  switch (d) {
+    case "+":
+      var c = a + b;
+      bb.innerHTML = "The sum of the two numbers is : " + c;
+      return c;
+      break;
+
+    case "-":
+      var c = a - b;
+      bb.innerHTML = "The difference of the two numbers is : " + c;
+      return c;
+      break;
+
+    case "/":
+      var c = a / b;
+      bb.innerHTML = "The division of the two numbers yields : " + c;
+      return c;
+      break;
+
+    case "*":
+      var c = a * b;
+      bb.innerHTML = "The multiplication of the two numbers is : " + c;
+      return c;
+      break;
+  }
 }
